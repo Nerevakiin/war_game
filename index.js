ecf631afab7f2df44b7e987e9f1358707ea2c119
Nerevakiin <djmatisk@yahoo.gr>
2025-12-09 00:25:37 +0200

    .

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var deckId
+
 function handleClick(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
+        .then(data => deckId = data.deck_id)
+}
+
+function drawCards() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
         .then(data => console.log(data))
 }
+
+document.getElementById('new-deck').addEventListener('click', handleClick)
+
+document.getElementById('draw-cards').addEventListener('click', drawCards)
