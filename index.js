e669022a50959bb1d1bb81cbbb55efc74ed6e770
Nerevakiin <djmatisk@yahoo.gr>
2025-12-11 01:55:33 +0200

    -

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var deckId
+const cardsContainer = document.getElementById('cards')
 
 function newDeckClick(){
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -15,15 +16,36 @@ function drawCards() {
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            document.getElementById('cards').children[0].innerHTML = `
-                <img src='${data.cards[0].image}' />
+            cardsContainer.children[0].innerHTML = `
+                <img src='${data.cards[0].image}' class='card' />
                 `
-            document.getElementById('cards').children[1].innerHTML = `
-                <img src='${data.cards[1].image}' />
+            cardsContainer.children[1].innerHTML = `
+                <img src='${data.cards[1].image}' class='card' />
             `
+
+            winnerCard(data.cards[0].value, data.cards[1].value)
         })
 }   else {
         console.log("You need to draw a deck first")
+    }
+}
+
+
+
+// Super proud I managed to think and write this all by myself hehe
+function winnerCard(card1, card2){
+        console.log(deckId)
+    console.log({card1}, {card2})
+    
+    const valueArray = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'JACK', 'QUEEN', 'KING', 'ACE']
+    
+    
+    if (valueArray.indexOf(card1) > valueArray.indexOf(card2)){
+        console.log("The winner is card1 with value: " + card1)
+    } else if ((valueArray.indexOf(card1) < valueArray.indexOf(card2))) {
+        console.log("The winner is card2 with value: " + card2)
+    } else {
+        console.log("Tie")
     }
 }
 
